81810f94c3fba61ae8190956e970eefe3f45da8e
Patrik Brocek <patrik.brocek@gmail.com>
2023-09-13 11:24:32 +0200

    Working on the second hypothesis

--- ages.py
+++ ages.py
@@ -1,6 +1,14 @@
-def categorize_ages(categories, data):
-    """
-    Categorize ages into categories
-    """
-    data["age"] = data["age"].apply(lambda x: categories[x])
+import numpy as np
+
+def categorize_age(interval, dato):
+    for i, age in enumerate(interval):
+        if interval[i] <= dato["age"] <= interval[i+1]:
+            dato["age"] = age
+            return dato
+
+def categorize_ages(interval, data):
+    # for each data categorize the age        
+    interval = np.append(interval, np.inf)
+    for dato in data:
+        dato = categorize_age(interval, dato)
     return data
--- test_delete.py
+++ test_delete.py
@@ -1,14 +1,5 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+a = range(10, 20)
 
-data_path = "dataset/supermarket/archive/customer_shopping_data.csv"
-data = pd.read_csv(data_path)
+for i, ii in enumerate(a):
+    print(i, ii)    
 
-clothing_data = data[data["category"] == "Clothing"]
-a = clothing_data["price"].unique()
-b = clothing_data["quantity"].unique()
-print(a)
-print(b)
-
-print(data["price"].unique().shape[0])
